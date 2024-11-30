d9f34c4d35a23a47196536684bbc915773d6b12a
Ilhom <khaitovilhom04@gmail.com>
2024-12-01 02:05:46 +0500

    added all files

--- src/routes/dashboard/Dashboard.tsx
+++ src/routes/dashboard/Dashboard.tsx
@@ -2,25 +2,27 @@ import {useState} from 'react';
 import {MenuFoldOutlined, MenuUnfoldOutlined,} from '@ant-design/icons';
 import {Button, Layout} from 'antd';
 import Sidebar from "../../components/sidebar/Sidebar.tsx";
-import {Outlet} from "react-router-dom";
+import {Outlet, useNavigate} from "react-router-dom";
 
 const {Header, Content} = Layout;
 
 const Dashboard = () => {
   const [collapsed, setCollapsed] = useState(false);
+  const navigate = useNavigate();
 
   return (
       <div>
         <Layout className="min-h-screen">
           <Sidebar collapsed={collapsed}/>
           <Layout>
-            <Header className="p-0 bg-white w-full min-h-[60px]">
+            <Header className="p-0 bg-white w-full min-h-[60px] flex justify-between items-center">
               <Button
                   type="text"
                   icon={collapsed ? <MenuUnfoldOutlined/> : <MenuFoldOutlined/>}
                   onClick={() => setCollapsed(!collapsed)}
                   className="text-[16px] !w-[64px] !h-[64px]"
               />
+              <Button className="mr-8" onClick={() => navigate("/auth")}>Login</Button>
             </Header>
             <Content className="bg-white rounded-xl shadow-xl overflow-hidden m-4 min-h-screen p-4">
               <Outlet/>
